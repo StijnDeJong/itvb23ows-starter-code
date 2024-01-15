2a7d502f308357a262932e484db7e455517a541b
stijn_de_jong <stijndejong125@gmail.com>
2024-01-15 17:17:26 +0100

    FEAT: Jenkinsfile updated to use sonar token with jenkins global credential

--- vars/sonarqubeAnalysis.groovy
+++ vars/sonarqubeAnalysis.groovy
@@ -1,7 +1,14 @@
 def call() {
-    echo "Running SonarQube analysis..."
-    withSonarQubeEnv('SonarQubeServer') {
-        // Execute SonarQube Scanner
-        sh "sonar-scanner"
+    def PROJECT_KEY = 'project1'
+    def SONAR_SCANNER_IMAGE = 'sonarsource/sonar-scanner-cli:latest'
+
+    node {
+        withCredentials([string(credentialsId: 'SONAR_TOKEN', variable: 'SONAR_TOKEN')]) {
+            docker.image(SONAR_SCANNER_IMAGE).inside {
+                withSonarQubeEnv('SonarQubeServer') {
+                    sh "sonar-scanner -Dsonar.projectKey=${PROJECT_KEY} -Dsonar.host.url=http://sonarqube:9000 -Dsonar.login=\${SONAR_TOKEN}"
+                }
+            }
+        }
     }
 }
