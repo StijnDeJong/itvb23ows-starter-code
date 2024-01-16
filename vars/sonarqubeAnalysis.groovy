6e535a6ad3c0da6399cb14b9cc484da22a52477c
stijn_de_jong <stijndejong125@gmail.com>
2024-01-16 17:00:43 +0100

    FEAT: Added sonar token to jenkinsfile

--- vars/sonarqubeAnalysis.groovy
+++ vars/sonarqubeAnalysis.groovy
@@ -2,10 +2,11 @@ def call() {
     def PROJECT_KEY = 'project1'
     def SONAR_SCANNER_IMAGE = 'sonarsource/sonar-scanner-cli:latest'
     def scannerHome = tool 'SonarQube Scanner'
+    def SONAR_TOKEN = credentials('SONAR_TOKEN')
 
     docker.image(SONAR_SCANNER_IMAGE).inside {
         withSonarQubeEnv('SonarQubeServer') {
-            sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=${PROJECT_KEY} -Dsonar.host.url=http://host.docker.internal:9000"
+            sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=${PROJECT_KEY} -Dsonar.host.url=http://host.docker.internal:9000 -Dsonar.login=${SONAR_TOKEN}"
         }
     }
 }
