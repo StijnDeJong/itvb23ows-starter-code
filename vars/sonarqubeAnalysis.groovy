b3a5b3c57d6dd4f153fae50967de4aedf1967164
stijn_de_jong <stijndejong125@gmail.com>
2024-01-16 17:14:39 +0100

    FIX: Use -e to set environment variables for the sonar-scanner command

--- vars/sonarqubeAnalysis.groovy
+++ vars/sonarqubeAnalysis.groovy
@@ -7,9 +7,11 @@ def call() {
     docker.image(SONAR_SCANNER_IMAGE).inside {
         withCredentials([string(credentialsId: 'SONAR_TOKEN', variable: 'SONAR_TOKEN')]) {
             sh """
-                export SONAR_HOST_URL=http://host.docker.internal:9000
-                export SONAR_LOGIN=${SONAR_TOKEN}
-                ${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=${PROJECT_KEY} -Dsonar.host.url=${SONAR_HOST_URL} -Dsonar.login=${SONAR_LOGIN}
+                docker run -e SONAR_HOST_URL=http://host.docker.internal:9000 \
+                           -e SONAR_LOGIN=${SONAR_TOKEN} \
+                           -v ${scannerHome}/bin/sonar-scanner:/sonar-scanner/bin/sonar-scanner \
+                           sonarsource/sonar-scanner-cli:latest \
+                           -Dsonar.projectKey=${PROJECT_KEY} -Dsonar.login=${SONAR_LOGIN} -Dsonar.host.url=${SONAR_HOST_URL}
             """
         }
     }
