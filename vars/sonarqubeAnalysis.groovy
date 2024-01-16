f64fc0a48d2b4e57e59294f20b738e123ad2fe04
stijn_de_jong <stijndejong125@gmail.com>
2024-01-16 14:50:00 +0100

    FIX: updated the sonar scan script

--- vars/sonarqubeAnalysis.groovy
+++ vars/sonarqubeAnalysis.groovy
@@ -1,14 +1,11 @@
 def call() {
     def PROJECT_KEY = 'project1'
     def SONAR_SCANNER_IMAGE = 'sonarsource/sonar-scanner-cli:latest'
+    def scannerHome = tool 'SonarQube Scanner'
 
-    node {
-        withCredentials([string(credentialsId: 'SONAR_TOKEN', variable: 'SONAR_TOKEN')]) {
-            docker.image(SONAR_SCANNER_IMAGE).inside {
-                withSonarQubeEnv('SonarQubeServer') {
-                    sh "sonar-scanner -Dsonar.projectKey=${PROJECT_KEY} -Dsonar.host.url=http://localhost:9000 -Dsonar.login=\${SONAR_TOKEN}"
-                }
-            }
+    docker.image(SONAR_SCANNER_IMAGE).inside {
+        withSonarQubeEnv('SonarQubeServer') {
+            sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=${PROJECT_KEY}"
         }
     }
 }
